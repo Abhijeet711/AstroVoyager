6794b8e208a256832fa16430a9c8b1883d3c6736
Abhijeet Bagwe <bagwe.abhijeet7@gmail.com>
2023-09-28 18:31:18 +0530

    Header, Footer and Libraries
    Added header, footer files. Also added all the required files to the project and updated the file system accordingly.

---
+++ includes/footer.php
@@ -0,0 +1,72 @@
+
+  <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 footer-info">
+                        <!-- put logo in src -->
+                        <img src="" alt="AstroVoyager Logo" style="height: 100px;
+                        width: 120px;
+                        margin-left: 30%;
+                        margin-right: 20%;
+                        margin-top: -10px;">
+                        <p style="color: #3D0066; font-size: 20px; margin-top: -10px; font-family: 'Fira Sans', sans-serif;" class="text-justify">
+                        <!-- put about astrovoyager content here! -->
+                        </p>
+                    </div>
+                    <div class="col-lg-4 col-md-12 footer-links">
+                        <h4 style="color: #3D0066; ">Useful Links</h4>
+                        <ul>
+                            <p>
+                                <!-- put images here -->
+                            <li><i class="bi bi-caret-right"></i> <a href="#" style="color: #3D0066; ">Home</a></li>
+                            <li><i class="bi bi-caret-right"></i> <a href="#" style="color: #3D0066; ">About us</a></li>
+                            <li><i class="bi bi-caret-right"></i> <a href="#" style="font-size:17px; color: #3D0066;">Images</a></li>
+                            </p>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 footer-contact " style="font-size:15px; ">
+                        <h4 style="color: #3D0066; ">Contact Us</h4>
+                        <p style="color: #3D0066; ">
+                        <!-- address below -->
+                            <!-- K J Somaiya Institute of Management, <br> Somaiya Vidyavihar University,<br>Vidyavihar East
+                            <br> Mumbai-400077. <br> -->
+                            <strong>Abhijeet Bagwe |</strong><a href="tel: +91-9405712796 " style="color: #3D0066; ">+91-8879054947</a><br>
+                            <strong>Renuka Ghadyalji |</strong><a href="tel: +91-9137928736 " style="color: #3D0066; ">+91-7678026693</a><br>
+                        </p>
+                        <div class="social-links ">
+                            <!-- just put link in href -->
+                            <a href="#" class="facebook " target="_blank "><i class="fa fa-facebook "></i></a>
+                            <a href="#" class="instagram " target="_blank "><i class="fa fa-instagram "></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container ">
+            <div class="copyright ">
+                &copy; Copyright <strong>AstroVoyager</strong>. All Rights Reserved.
+            </div>
+        </div>
+    </footer>
+    <!-- #footer -->
+
+    <a href="# " class="back-to-top " style="background-color: #3D0066 ; "><i class="fa fa-angle-up "></i></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="assets/lib/jquery/jquery.min.js "></script>
+    <script src="assets/lib/jquery/jquery-migrate.min.js "></script>
+    <script src="assets/lib/bootstrap/js/bootstrap.bundle.min.js "></script>
+    <script src="assets/lib/easing/easing.min.js "></script>
+    <script src="assets/lib/superfish/hoverIntent.js "></script>
+    <script src="assets/lib/superfish/superfish.min.js "></script>
+    <script src="assets/js/script.js"></script>
+    <!-- Template Main Javascript File -->
+    <script src="assets/js/main.js "></script>
+    <script src="assets/js/event-carousel.js "></script>
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="utf-8">
+    <title !important>Login Page!</title>
+</head>
+<?php
+include 'includes/header.php';
+?>
+
+<?php
+include 'includes/footer.php';
+?>
