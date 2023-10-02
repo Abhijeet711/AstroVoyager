5b6567a63b5cf88c5225989ec3cc7208687453a1
Abhijeet Bagwe <bagwe.abhijeet7@gmail.com>
2023-10-02 21:58:31 +0530

    PHP Header and Footer
    Fixed some issues where local host couldnt find path for the linked css and js files.

--- includes/footer.php
+++ includes/footer.php
@@ -1,72 +1,99 @@
 
-  <footer id="footer">
-        <div class="footer-top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 footer-info">
-                        <!-- put logo in src -->
-                        <img src="" alt="AstroVoyager Logo" style="height: 100px;
-                        width: 120px;
-                        margin-left: 30%;
-                        margin-right: 20%;
-                        margin-top: -10px;">
-                        <p style="color: #3D0066; font-size: 20px; margin-top: -10px; font-family: 'Fira Sans', sans-serif;" class="text-justify">
-                        <!-- put about astrovoyager content here! -->
-                        </p>
-                    </div>
-                    <div class="col-lg-4 col-md-12 footer-links">
-                        <h4 style="color: #3D0066; ">Useful Links</h4>
-                        <ul>
-                            <p>
-                                <!-- put images here -->
-                            <li><i class="bi bi-caret-right"></i> <a href="#" style="color: #3D0066; ">Home</a></li>
-                            <li><i class="bi bi-caret-right"></i> <a href="#" style="color: #3D0066; ">About us</a></li>
-                            <li><i class="bi bi-caret-right"></i> <a href="#" style="font-size:17px; color: #3D0066;">Images</a></li>
-                            </p>
-                        </ul>
-                    </div>
+<footer id="footer" style="margin-top:-4% !important;">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 footer-info">
+            <!-- put logo in src -->
+            <img src="../assets/img/astro logo.png"" alt="AstroVoyager Logo"
+            style=" height: 100px; width: 100px; margin-left: 30%; margin-right:
+            20%; margin-top: -10px; " />
+            <p
+              style="
+                color: #fdc500;
+                font-size: 16px;
+                width: 90%;
+                margin-top: 5px;
+              "
+              class="text-justify"
+            >
+              AstroVoyager aims to revolutionize commercial space travel by
+              providing suborbital spaceflights to the public. It also strives
+              to promote and support the mission of making space travel
+              accessible to civilians and researchers.
+            </p>
+          </div>
+          <div class="col-lg-4 col-md-12 footer-links">
+            <h4 style="color: #fdc500">Useful Links</h4>
+            <ul>
+              <p>
+                <li>
+                  <i class="bi bi-caret-right"></i>
+                  <a href="#" style="color: #fdc500">Home</a>
+                </li>
+                <li>
+                  <i class="bi bi-caret-right"></i>
+                  <a href="#" style="color: #fdc500">About us</a>
+                </li>
+                <li>
+                  <i class="bi bi-caret-right"></i>
+                  <a href="#" style="font-size: 17px; color: #fdc500">Images</a>
+                </li>
+              </p>
+            </ul>
+          </div>
 
-                    <div class="col-lg-4 col-md-6 footer-contact " style="font-size:15px; ">
-                        <h4 style="color: #3D0066; ">Contact Us</h4>
-                        <p style="color: #3D0066; ">
-                        <!-- address below -->
-                            <!-- K J Somaiya Institute of Management, <br> Somaiya Vidyavihar University,<br>Vidyavihar East
-                            <br> Mumbai-400077. <br> -->
-                            <strong>Abhijeet Bagwe |</strong><a href="tel: +91-9405712796 " style="color: #3D0066; ">+91-8879054947</a><br>
-                            <strong>Renuka Ghadyalji |</strong><a href="tel: +91-9137928736 " style="color: #3D0066; ">+91-7678026693</a><br>
-                        </p>
-                        <div class="social-links ">
-                            <!-- just put link in href -->
-                            <a href="#" class="facebook " target="_blank "><i class="fa fa-facebook "></i></a>
-                            <a href="#" class="instagram " target="_blank "><i class="fa fa-instagram "></i></a>
-                        </div>
-                    </div>
-                </div>
+          <div class="col-lg-4 col-md-6 footer-contact" style="font-size: 15px">
+            <h4 style="color: #fdc500">Contact Us</h4>
+            <p style="color: #fdc500">
+              <!-- address below -->
+              K J Somaiya Institute of Management, <br> Somaiya Vidyavihar University,<br>Vidyavihar East, Mumbai-400077. <br>
+              <strong>Abhijeet Bagwe |</strong
+              ><a href="tel: +91-9405712796 " style="color: #fdc500"
+                >+91-8879054947</a
+              ><br />
+              <strong>Renuka Ghadyalji |</strong
+              ><a href="tel: +91-9137928736 " style="color: #fdc500"
+                >+91-7678026693</a
+              ><br />
+            </p>
+            <div class="social-links">
+              <!-- just put link in href -->
+              <a href="#" class="facebook" target="_blank "
+                ><i class="fa fa-facebook"></i
+              ></a>
+              <a href="#" class="instagram" target="_blank "
+                ><i class="fa fa-instagram"></i
+              ></a>
             </div>
+          </div>
         </div>
-
-        <div class="container ">
-            <div class="copyright ">
-                &copy; Copyright <strong>AstroVoyager</strong>. All Rights Reserved.
-            </div>
-        </div>
-    </footer>
-    <!-- #footer -->
-
-    <a href="# " class="back-to-top " style="background-color: #3D0066 ; "><i class="fa fa-angle-up "></i></a>
-
-    <!-- JavaScript Libraries -->
-    <script src="assets/lib/jquery/jquery.min.js "></script>
-    <script src="assets/lib/jquery/jquery-migrate.min.js "></script>
-    <script src="assets/lib/bootstrap/js/bootstrap.bundle.min.js "></script>
-    <script src="assets/lib/easing/easing.min.js "></script>
-    <script src="assets/lib/superfish/hoverIntent.js "></script>
-    <script src="assets/lib/superfish/superfish.min.js "></script>
-    <script src="assets/js/script.js"></script>
-    <!-- Template Main Javascript File -->
-    <script src="assets/js/main.js "></script>
-    <script src="assets/js/event-carousel.js "></script>
-
-</body>
-
+      </div>
+    </div>
+    <div class="container">
+      <div class="copyright" style="margin-top: -2%">
+        &copy; Copyright <strong>AstroVoyager</strong>. All Rights Reserved.
+      </div>
+    </div>
+</footer>
+    <!-- Bootstrap CSS File -->
+    <link href="../assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- link below is for triangles in footer -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+    />
+    <!-- Main Stylesheet File -->
+    <link href="../assets/css/styles.css" rel="stylesheet" />
+    <!-- link below is for icons of instagram and facebook in footer  -->
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+<script src="../assets/lib/jquery/jquery.min.js "></script>
+<script src="../assets/lib/jquery/jquery-migrate.min.js "></script>
+<script src="../assets/lib/bootstrap/js/bootstrap.bundle.min.js "></script>
+<script src="../assets/lib/easing/easing.min.js "></script>
+<script src="../assets/lib/superfish/hoverIntent.js "></script>
+<script src="../assets/lib/superfish/superfish.min.js "></script>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <title>AstroVoyager</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+<html>
+    <head>
+    <!-- Bootstrap CSS File -->
+    <link href="../assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- link below is for triangles in footer -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+    />
+    <!-- Main Stylesheet File -->
+    <link href="../assets/css/styles.css" rel="stylesheet" />
+    <!-- link below is for icons of instagram and facebook in footer  -->
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
     <!-- TAILWIND -->
     <link
       rel="stylesheet"
@@ -18,181 +27,31 @@
       crossorigin="anonymous"
       referrerpolicy="no-referrer"
     ></script>
-    <!-- Favicons -->
-    <!-- put logo link in href -->
-    <link href="assets/img/#" rel="icon">
-    <link href="assets/img/#" rel="apple-touch-icon">
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
-
-    <!-- Bootstrap CSS File -->
-    <link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.min.js"
-      integrity="sha512-WW8/jxkELe2CAiE4LvQfwm1rajOS8PHasCCx+knHG0gBHt8EXxS6T6tJRTGuDQVnluuAvMxWF4j8SNFDKceLFg=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    ></script>
-
-    <!-- Libraries CSS Files -->
-    <link href="assets/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-
-    <!-- Main Stylesheet File -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Marcellus&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap" rel="stylesheet">
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link href="assets/css/event-carousel.css" rel="stylesheet">
-
-    <style>
-        /* #intro {
-            width: 100%;
-            height: 140vh;
-            background: #fff;
-            background-size: cover;
-            overflow: hidden;
-            position: relative;
-        }
-
-        .landing-inner {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: #fff;
-            text-align: center;
-            padding-top: 30px;
-        }
-
-        .landing {
-            position: relative;
-            background-size: cover;
-            background-position: center;
-            height: 100vh;
-        }
-
-        .landing h1 {
-            font-size: 50px;
-        }
-
-        .landing p {
-            font-size: 20px;
-        }
-
-        .countdown.mobile-nav-active {
-            font-size: 50vw;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-        }
-
-        .countdown {
-            font-size: 5vw;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            text-align: center;
-        }
-
-        .countdown div {
-            padding: 1vw;
-            border: 1px #fff solid;
-            border-radius: 2em;
-            background: #FFF;
-            opacity: 0.7;
-            margin: 0.5rem;
-        }
-
-        .countdown div:first-child {
-            background: #FFF;
-        }
-
-        .countdown span {
-            display: block;
-            font-size: 3vh;
-        }
-
-        @media (max-width: 350px) {
-            .landing img {
-                width: 70%;
-            }
-
-            .landing h1 {
-                font-size: 40px;
-            }
-
-            .countdown {
-                font-size: 5vw;
-                flex-direction: column;
-            }
-
-            .countdown div {
-                display: none;
-            }
-
-            .countdown div:first-child {
-                display: block;
-                width: 50%;
-                padding: 2rem;
-            }
-        } */
-
-        #header #logo img {
-            position: absolute;
-            left: 3%;
-            padding: 3px;
-            margin: auto;
-            height: 80px;
-            top: 12%;
-        }
-
-        #nav-menu-container {
-            float: right;
-            margin: 0;
-            margin-right: -35px;
-        }
-
-        @media (min-width: 992px) {
-            .venoframe,
-            .vbox-inline {
-                max-width: 1200px;
-                width: 63%;
-                height: 540px;
-                height: 70vh;
-            }
-        }
-
-        #header {
-            height: 90px;
-        }
-    </style>
-</head>
-
-<body>
-    <header id="header">
-    <div class="container">
-      <div id="logo" class="pull-left">
-        <!-- put link to landing page in <a href> -->
-        <!-- put link to image in img src -->
-        <a href="#" class="scrollto"
-          ><img src="assets/background.jpg" alt="" title=""
-        /></a>
-      </div>
-      <nav id="nav-menu-container">
-        <ul class="nav-menu">
-          <!-- link to home landing page -->
-          <li class="menu-active"><a href="#">Home</a></li>
-          <!-- link to about us page -->
-          <li><a href="#">About Us</a></li>
-          <!-- link to images page -->
-          <li><a href="#">Images</a></li>
-          <!-- link to login/registration page -->
-          <li><a href="#">Login</a></li>
-          <!-- put link to img in src -->
-        </ul>
-      </nav>
-    </div>
-  </header>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Astro Voyager</title>
+  </head>
+  <header id="header">
+        <div class="container">
+        <div id="logo" class="pull-left">
+            <!-- put link to landing page in <a href> -->
+            <!-- put link to image in img src -->
+            <a href="#" class="scrollto"
+            ><img src="../assets/img/astro logo.png" alt="" title=""
+            /></a>
+        </div>
+        <nav id="nav-menu-container">
+            <ul class="nav-menu">
+            <!-- link to home landing page -->
+            <li class="menu-active"><a href="#">Home</a></li>
+            <!-- link to about us page -->
+            <li><a href="#">About Us</a></li>
+            <!-- link to images page -->
+            <li><a href="#">Images</a></li>
+            <!-- link to login/registration page -->
+            <li><a href="#">Login</a></li>
+            <!-- put link to img in src -->
+            </ul>
+        </nav>
+        </div>
+    </header>
